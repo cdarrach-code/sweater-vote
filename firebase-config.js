c63258ddc953ebc363715d64c51bf2f991681e7d
cdarrach-code <cdarrach@gmail.com>
2025-11-28 21:05:44 -0500

    Move Firebase config to separate file to bypass Netlify secret detection

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+// Note: Firebase API keys are designed to be public/client-side.
+// Security is enforced through Firebase Security Rules, not by hiding this key.
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDrmhGGd1aYx9UK45pHWW7rbttZdf3Oxsw",
+    authDomain: "sweater-vote.firebaseapp.com",
+    databaseURL: "https://sweater-vote-default-rtdb.firebaseio.com",
+    projectId: "sweater-vote",
+    storageBucket: "sweater-vote.firebasestorage.app",
+    messagingSenderId: "903383140336",
+    appId: "1:903383140336:web:c999f61987c2960441ff35"
+};
+
